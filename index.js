db0d2fed6ba258eef1be0ede77a1d56496a7bef0
Kristina Alekyan <kristinaalekyan1@gmail.com>
2020-06-11 00:51:36 +0400

    changing Grid to Table with onClick

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let previouslyToggled=document.getElementById("content1");
+function changeContent(view){
+    let result = "content" + view.toString();
+    let x = document.getElementById(result);
+  
+  if (previouslyToggled && previouslyToggled !== x) {
+    previouslyToggled.style.display = "none"; 
+  }
+  
+  if (x.style.display === "flex") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "flex";
+    
+  }
+  
+  previouslyToggled = x;
+}
