8bd5deb52c50e02eb17273477a2b98f7ba832277
Kristina Alekyan <kristinaalekyan1@gmail.com>
2020-06-13 04:54:19 +0400

    pending page with media

--- index.js
+++ index.js
@@ -15,21 +15,3 @@ function changeContent(view){
   }  
   previouslyToggled = x;
 }
-
-
-function changeActiveToPending(view){
-    let result = "content" + view.toString();
-    let x = document.getElementById(result);
-    let z = document.getElementsByClassName(result);
-  
-  if (previouslyToggled && previouslyToggled !== x) {
-    previouslyToggled.style.display = "none"; 
-  }
-  
-  if (x.style.display === "flex") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "flex";    
-  }  
-  previouslyToggled = x;
-}
